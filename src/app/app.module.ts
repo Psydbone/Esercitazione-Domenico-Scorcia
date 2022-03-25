7bdcf00b6eb8a175f0ea7f772d1f4f9cc507de18
Psydbone <d.scorcia@studenti.poliba.it>
2022-03-25 09:24:21 +0100

    4 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,12 @@ import { HelloComponent } from './hello.component';
 import { TestModule } from './test.module';
 import { TestService, TestService2 } from './test.service';
 import { TestDirective } from './test.directive';
-import { arrayDirettiva } from './array.directive';
 import { TableComponent } from './table.component';
 import { ArrayService } from './array.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ,TestModule],
-  declarations: [ AppComponent, HelloComponent, TestDirective,arrayDirettiva,TableComponent],
+  declarations: [ AppComponent, HelloComponent, TestDirective,TableComponent],
   providers : [TestService,TestService2,ArrayService],
   bootstrap:    [ AppComponent ]
 })
--- src/app/table.component.ts
+++ src/app/table.component.ts
@@ -4,6 +4,7 @@ import { ArrayService, Person } from './array.service';
 @Component({
   selector: 'table-component',
   templateUrl: `./table.template.html`,
+  styleUrls:['./table.component.css']
 })
 export class TableComponent {
   data: Person[];
